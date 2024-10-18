365ca960f95227d0480dc217d5638645bad68d24
Jonathan V <jonathanvieri1321@gmail.com>
2024-10-18 16:16:30 +0700

    Initial Commit

---
+++ Horizon/HorizonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HorizonApp.swift
+//  Horizon
+//
+//  Created by Jonathan Vieri on 17/10/24.
+//
+
+import SwiftUI
+
+@main
+struct HorizonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
