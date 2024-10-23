f55230e12a83d98939a3ad7144a9378c022b1ba4
Jonathan V <jonathanvieri1321@gmail.com>
2024-10-23 17:09:52 +0700

    add weather data

---
+++ Horizon/Model/WeatherData.swift
@@ -0,0 +1,53 @@
+//
+//  WeatherData.swift
+//  Horizon
+//
+//  Created by Jonathan Vieri on 23/10/24.
+//
+
+import Foundation
+
+struct WeatherData : Decodable {
+    let name: String
+    let weather: [Weather]
+    let main: Main
+    let wind: Wind
+    let sys: Sys
+}
+
+//MARK: - Weather
+struct Weather : Decodable {
+    let id: Int
+    let description: String
+}
+
+//MARK: - Main
+struct Main : Decodable {
+    let temp: Double
+    let tempMin: Double
+    let tempMax: Double
+    let feelsLike: Double
+    let humidity: Int
+    
+    // Use CodingKeys to map the JSON keys to Swift properties
+    enum CodingKeys: String, CodingKey {
+        case temp
+        case tempMin = "temp_min"
+        case tempMax = "temp_max"
+        case feelsLike = "feels_like"
+        case humidity
+    }
+}
+
+//MARK: - Wind
+struct Wind : Decodable {
+    let speed: Double
+    let deg: Int
+    let gust: Double
+}
+
+//MARK: - Sys
+struct Sys : Decodable {
+    let sunrise: Double
+    let sunset: Double
+}
