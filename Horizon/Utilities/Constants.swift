955fbe602dacfe79ca9e6f059e6f8705d4b8a88e
Jonathan V <jonathanvieri1321@gmail.com>
2024-10-27 14:28:01 +0700

    Update constants
    Added new color for DarkColors and added a new struct called AQIColors for AQI related color palette

--- Horizon/Utilities/Constants.swift
+++ Horizon/Utilities/Constants.swift
@@ -13,20 +13,20 @@ struct K {
     static let apiKey = "YOUR_API_KEY_HERE"
     static let baseUrl = "https://api.openweathermap.org/data/2.5"
     
-    // Temperature Units
-    static let celsiusUnit = "metric"
-    static let fahrenheitUnit = "imperial"
-    
-    // Default Location
-    static let defaultCity = "Jakarta"
-    
-    
     // Color Palette
     struct DarkColors {
         static let background = Color(red: 0.10, green: 0.11, blue: 0.12)
         static let white = Color(red: 0.98, green: 0.99, blue: 1)
         static let gray = Color(red: 0.17, green: 0.18, blue: 0.18)
         static let blue = Color(red: 0.52, green: 0.80, blue: 0.92)
+        static let yellow = Color(red: 0.98, green: 0.74, blue: 0.28)
     }
     
+    struct AQIColors {
+        static let purple = Color(red: 0.75, green: 0.51, blue: 0.98)
+        static let red = Color(red: 0.90, green: 0.11, blue: 0.21)
+        static let orange = Color(red: 0.98, green: 0.47, blue: 0.12)
+        static let yellow = Color(red: 1, green: 0.87, blue: 0.40)
+        static let green = Color(red: 0.60, green: 0.77, blue: 0.23)
+    }
 }
